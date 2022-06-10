ef7976746cbccb2c27f59a1775dc71435ede2806
DavideIsaia <davide.enjoy@hotmail.it>
2022-06-10 14:46:36 +0200

    milestone 1 raggiunta

--- api/index-json.php
+++ api/index-json.php
@@ -4,7 +4,11 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Vue Dischi</title>
+  <!-- Axios -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.27.2/axios.min.js" integrity="sha512-odNmoc1XJy5x1TMVMdC7EMs3IVdItLPlCeL5vSUPN2llYKMJ2eByTTAIiiuqLg+GdNr9hF6z81p27DArRFKT7A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <!-- VueJs -->
+  <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
 </head>
 <body>
   <div id="root">
--- index.php
+++ index.php
@@ -5,9 +5,39 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Vue Dischi</title>
-  <link rel="stylesheet" href="css/style.css">
+  <!-- bootstrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <!-- style -->
+  <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
+  <!-- includo il database con require perchè è un elemento importante -->
+  <?php require_once __DIR__ . "/database.php"; ?>
   
+  <!-- header -->
+  <header>
+    <a href="https://www.spotify.com/">
+      <img src="./img/spotify-download-logo-30.png" alt="Spotify logo" />
+    </a>
+  </header>
+  
+  <!-- main -->
+  <main>
+    <div class="container">
+      <div class="row row-cols-3 row-cols-lg-6 mt-4">
+        <!-- creo le varie card usando il ciclo foreach -->
+        <?php foreach ($database as $element) { ?>
+          <div class="my-card text-center p-3 mb-3 mx-3">
+            <img src="<?php echo $element["poster"]; ?>" alt="<?php echo $element["title"]; ?>"/>
+            <h2 class="p-3"><?php echo $element["title"]; ?></h2>
+            <h3><?php echo $element["author"]; ?></h3>
+            <h3><?php echo $element["year"]; ?></h3>
+          </div>         
+          <?php 
+        }       
+        ?>
+      </div>
+    </div>    
+  </main>
 </body>
 </html>
